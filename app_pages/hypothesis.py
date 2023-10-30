3a2c0976a92beec4871ac7fb9d91e13a48268bd8
fatimamahate <>
2023-10-30 08:46:07 +0000

    Add app pages

---
+++ app_pages/hypothesis.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def hypothesis_ml():
+    st.write("##The project hypothesis")
+
+    st.info(
+        "* The leaves affected with mildew will visibly look different.\n"
+        f"It will have a powdery layer on the leaf which will be detected."
+        )
